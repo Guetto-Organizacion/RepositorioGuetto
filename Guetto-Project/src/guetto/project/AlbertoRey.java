bdd2160348258e3faada7ae767009f1ac96aed75
DAM1 <DAM1@CFA4-p01>
2016-12-07 19:43:35 +0100

    Subiendo ejercicio a la branch de Alberto

---
+++ Guetto-Project/src/guetto/project/AlbertoRey.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guetto.project;
+
+/**
+ *
+ * @author DAM1
+ */
+public class AlbertoRey {
+
+    public static void main(String[] args) {
+        
+        int a = 2, b = 4;
+        int x, y, z;
+        
+        x=-a+5%b-a*a;
+        System.out.println("A) -a+5%b-a*a = " + x);
+        y=5+3%7*b*a-b%a;
+        System.out.println("B) 5+3%7*b*a-b%a = " + y);
+        z=(a+1)*(b+1)-b/a;
+        System.out.println("C) a+1)*(b+1)-b/a = " + z);
+        
+    }
+    
+}
